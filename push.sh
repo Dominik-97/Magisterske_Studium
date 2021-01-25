06d8a3d17c7378b7067aa712be34b0a1f6e2b260
Dominik-97 <58784420+Dominik-97@users.noreply.github.com>
2021-01-25 16:58:54 +0100

    Edited push.sh

--- push.sh
+++ push.sh
@@ -5,9 +5,12 @@ git fetch && git pull
 
 echo "Checking if there is something changed in the repository"
 if ! git diff-index --quiet HEAD --; then
-    read -p "Please specify what to commit the same way you would after git add." whattoCommit
+    git status
+    cr=`echo $'\n.'`
+    cr=${cr%.}
+    read -p "Please specify what to commit the same way you would after git add. $cr" whattoCommit
     git add $whattoCommit
-    read -p "What is your commit message?" commitMessage
+    read -p "What is your commit message? $cr" commitMessage
     git commit -m "$commitMessage"
     git push
 fi
